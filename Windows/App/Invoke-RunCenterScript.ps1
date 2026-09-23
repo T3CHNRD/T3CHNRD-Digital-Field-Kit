193b9d60d03e09f7182ea497a4dc585c4af6108f
Tim Vedders <tim@careernowbrands.com>
2026-09-23 11:05:16 -0400

    Handle UTF-8 BOM in Run Center redirected input

--- Windows/App/Invoke-RunCenterScript.ps1
+++ Windows/App/Invoke-RunCenterScript.ps1
@@ -13,6 +13,7 @@ function global:Read-Host {
     [Console]::Out.Flush()
     $answer=[Console]::In.ReadLine()
     if($null -eq $answer){throw 'Run Center input stream was closed.'}
+    if($answer.Length -gt 0 -and [int]$answer[0] -eq 0xFEFF){$answer=$answer.Substring(1)}
     if($AsSecureString){
         $secure=New-Object Security.SecureString
         foreach($character in $answer.ToCharArray()){$secure.AppendChar($character)}
